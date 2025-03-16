85722dcd92ebd34edfe99005f4fb84ef73ceb63c
machlarz <mimac1982@gmail.com>
2025-03-16 12:11:36 +0100

    my changes

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +22,36 @@ public class ParticipantService {
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
 	}
+
+	public Participant findByLogin(String login) {
+		String hql = "FROM Participant WHERE login = :login";
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("login", login);
+		return (Participant) query.uniqueResult();
+	}
+
+	public Participant addParticipant(Participant participant) {
+		Transaction tx = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		tx.commit();
+		return participant;
+	}
+
+	public Participant removeParticipant(Participant participant) {
+		Transaction tx = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		tx.commit();
+		return participant;
+	}
+
+	public Participant updateParticipant(Participant participant, String login) {
+		Transaction tx = connector.getSession().beginTransaction();
+		Participant participant1 = connector.getSession().get(Participant.class, login);
+		participant1.setPassword(participant.getPassword());
+		connector.getSession().save(participant1);
+		tx.commit();
+		return participant;
+	}
+
 
 }
